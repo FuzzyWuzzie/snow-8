1bb644e01688b30ae7c789c5006a3e7b35b76f39
Kenton Hamaluik <kenton@hamaluik.com>
2016-02-29 21:45:04 -0700

    Initialized empty project

---
+++ src/Main.hx
@@ -0,0 +1,31 @@
+import snow.api.Debug.*;
+import snow.types.Types;
+import snow.modules.opengl.GL;
+
+typedef UserConfig = {}
+
+@:log_as('app')
+class Main extends snow.App {
+    function new() {}
+
+    override function config(config:AppConfig): AppConfig {
+        config.window.title = 'snow-8';
+        return config;
+    }
+
+    override function ready() {
+        log('ready');
+    }
+
+    override function onkeyup(keycode:Int, _,_, mod:ModState, _,_) {
+        if(keycode == Key.escape) {
+            app.shutdown();
+        }
+    } 
+
+    override function tick(delta:Float) {
+        GL.clearColor(1.0, 1.0, 1.0, 1.0);
+        GL.clear(GL.COLOR_BUFFER_BIT);
+    }
+
+}
