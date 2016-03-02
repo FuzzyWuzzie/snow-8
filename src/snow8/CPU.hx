27424d1b43a2cbeba8e98ce0aec5fd46cd50f025
Kenton Hamaluik <kenton@hamaluik.com>
2016-03-01 22:29:53 -0700

    Added tracing of opcodes, basic processing compatibility

--- src/snow8/CPU.hx
+++ src/snow8/CPU.hx
@@ -3,6 +3,7 @@ package snow8;
 import haxe.ds.Vector;
 import haxe.ds.GenericStack;
 import snow.api.buffers.Uint8Array;
+import debug.Log;
 using StringTools;
 
 class CPU {
@@ -29,54 +30,68 @@ class CPU {
 		switch(opcode & 0xF000) {
 			case OpCodes.GRP_SYS: {
 				switch(opcode) {
-					case OpCodes.CLS: display.clear_screen();
+					case OpCodes.CLS: {
+						display.clear_screen();
+						Log.trace('cls');
+					}
+
 					case OpCodes.RET: {
 						var addr:Int = stack.pop();
 						memory.program_counter = addr;
+						Log.trace('ret -> ${addr}');
 					}
-					case _: {}
+					case _: {
+						Log.trace('0x0000');
+					}
 				}
 			}
 
 			case OpCodes.JP_ADDR: {
 				var addr:Int = opcode & 0x0FFF;
 				memory.program_counter = addr;
+				Log.trace('jp_addr ${addr}');
 			}
 
 			case OpCodes.CALL_ADDR: {
 				var addr:Int = opcode & 0x0FFF;
 				stack.add(memory.program_counter);
 				memory.program_counter = addr;
+				Log.trace('call_addr ${addr}');
 			}
 
 			case OpCodes.SE_BYTE: {
 				var reg:Int = (opcode & 0x0F00) >> 8;
 				var byte:Int = opcode & 0x00FF;
 				if(registers[reg] == byte) memory.program_counter += 2;
+				Log.trace('se_byte [${reg}] == ${byte}');
 			}
 
 			case OpCodes.SNE_BYTE: {
 				var reg:Int = (opcode & 0x0F00) >> 8;
 				var byte:Int = opcode & 0x00FF;
 				if(registers[reg] != byte) memory.program_counter += 2;
+				Log.trace('sne_byte [${reg}] == ${byte}');
 			}
 
 			case OpCodes.SE_REG: {
 				var reg_x:Int = (opcode & 0x0F00) >> 8;
 				var reg_y:Int = (opcode & 0x00F0) >> 4;
 				if(registers[reg_x] == registers[reg_y]) memory.program_counter += 2;
+				Log.trace('se_reg [${reg_x}] == [${reg_y}]');
 			}
 
 			case OpCodes.LD_BYTE: {
 				var reg:Int = (opcode & 0x0F00) >> 8;
 				var val:Int = opcode & 0x00FF;
 				registers[reg] = val;
+				Log.trace('ld_byte [${reg}] = ${val}');
 			}
 
 			case OpCodes.ADD_BYTE: {
 				var reg:Int = (opcode & 0x0F00) >> 8;
 				var val:Int = opcode & 0x00FF;
 				registers[reg] = 0x0FF & (registers[reg] + val);
+				Log.trace('add_byte [${reg}] + ${val}');
 			}
 
 			case OpCodes.GRP_MATH: {
@@ -85,24 +100,28 @@ class CPU {
 						var reg_x:Int = (opcode & 0x0F00) >> 8;
 						var reg_y:Int = (opcode & 0x00F0) >> 4;
 						registers[reg_x] = registers[reg_y];
+						Log.trace('ld_reg [${reg_x}] = [${reg_y}]');
 					}
 
 					case OpCodes.OR_REG: {
 						var reg_x:Int = (opcode & 0x0F00) >> 8;
 						var reg_y:Int = (opcode & 0x00F0) >> 4;
 						registers[reg_x] = registers[reg_x] | registers[reg_y];
+						Log.trace('or_reg [${reg_x}] | [${reg_y}]');
 					}
 
 					case OpCodes.AND_REG: {
 						var reg_x:Int = (opcode & 0x0F00) >> 8;
 						var reg_y:Int = (opcode & 0x00F0) >> 4;
 						registers[reg_x] = registers[reg_x] & registers[reg_y];
+						Log.trace('and_reg [${reg_x}] & [${reg_y}]');
 					}
 
 					case OpCodes.XOR_REG: {
 						var reg_x:Int = (opcode & 0x0F00) >> 8;
 						var reg_y:Int = (opcode & 0x00F0) >> 4;
 						registers[reg_x] = registers[reg_x] ^ registers[reg_y];
+						Log.trace('xor_reg [${reg_x}] ^ [${reg_y}]');
 					}
 
 					case OpCodes.ADD_REG: {
@@ -111,6 +130,7 @@ class CPU {
 						registers[reg_x] += registers[reg_y];
 						registers[0x0f] = if(registers[reg_x] > 255) 1 else 0;
 						registers[reg_x] = registers[reg_x] & 0xff;
+						Log.trace('add_reg [${reg_x}] + [${reg_y}]');
 					}
 
 					case OpCodes.SUB_REG: {
@@ -119,12 +139,14 @@ class CPU {
 						registers[0x0f] = if(registers[reg_x] > registers[reg_y]) 1 else 0;
 						registers[reg_x] -= registers[reg_y];
 						registers[reg_x] = registers[reg_x] & 0xff;
+						Log.trace('sub_reg [${reg_x}] - [${reg_y}]');
 					}
 
 					case OpCodes.SHR_REG: {
 						var reg_x:Int = (opcode & 0x0F00) >> 8;
 						registers[0x0f] = registers[reg_x] & 0x01;
 						registers[reg_x] = 0xff & (registers[reg_x] >>> 1);
+						Log.trace('shr_reg [${reg_x}]');
 					}
 
 					case OpCodes.SUBN_REG: {
@@ -132,12 +154,14 @@ class CPU {
 						var reg_y:Int = (opcode & 0x00F0) >> 4;
 						registers[0x0f] = if(registers[reg_y] > registers[reg_x]) 1 else 0;
 						registers[reg_x] = 0xff & (registers[reg_y] - registers[reg_x]);
+						Log.trace('SUBN_REG [${reg_y}] - [${reg_x}]');
 					}
 
 					case OpCodes.SHL_REG: {
 						var reg_x:Int = (opcode & 0x0F00) >> 8;
 						registers[0x0f] = (registers[reg_x] & 0x80) >> 7;
 						registers[reg_x] = 0xff & (registers[reg_x] << 1);
+						Log.trace('shl_reg [${reg_x}]');
 					}
 
 					case _: {
@@ -150,16 +174,19 @@ class CPU {
 				var reg_x:Int = (opcode & 0x0F00) >> 8;
 				var reg_y:Int = (opcode & 0x00F0) >> 4;
 				if(registers[reg_x] != registers[reg_y]) memory.program_counter += 2;
+				Log.trace('sne_reg [${reg_x}] != [${reg_y}');
 			}
 
 			case OpCodes.LD_I_ADDR: {
 				var val:Int = opcode & 0x0FFF;
 				index_register = val;
+				Log.trace('ld_i_addr = ${val}');
 			}
 
 			case OpCodes.JP_ADDR_OFFS: {
 				var addr:Int = opcode & 0x0FFF;
 				memory.program_counter = addr + registers[0];
+				Log.trace('jp_addr_offs ${addr}');
 			}
 
 			case OpCodes.RND_BYTE: {
@@ -167,6 +194,7 @@ class CPU {
 				var byte:Int = opcode & 0xff;
 				var rand:Int = Math.floor((256 * Math.random()));
 				registers[reg_x] = byte & rand;
+				Log.trace('rnd_byte [${reg_x}] = ${byte} & ${rand}');
 			}
 
 			case OpCodes.GRP_SKP: {
@@ -174,11 +202,13 @@ class CPU {
 					case OpCodes.SKP_REG: {
 						var reg_x:Int = (opcode & 0x0F00) >> 8;
 						if(input.is_key_pressed(registers[reg_x])) memory.program_counter += 2;
+						Log.trace('skp_reg [${reg_x}]');
 					}
 
 					case OpCodes.SKNP_REG: {
 						var reg_x:Int = (opcode & 0x0F00) >> 8;
 						if(!input.is_key_pressed(registers[reg_x])) memory.program_counter += 2;
+						Log.trace('sknp_reg [${reg_x}]');
 					}
 
 					case _: {
@@ -192,21 +222,25 @@ class CPU {
 					case OpCodes.LD_REG_DT: {
 						var reg_x:Int = (opcode & 0x0F00) >> 8;
 						registers[reg_x] = timers.delay_timer;
+						Log.trace('ld_reg_dt [${reg_x}]');
 					}
 
 					case OpCodes.LD_DT_REG: {
 						var reg_x:Int = (opcode & 0x0F00) >> 8;
 						timers.delay_timer = registers[reg_x];
+						Log.trace('ld_dt_reg [${reg_x}]');
 					}
 
 					case OpCodes.LD_ST_REG: {
 						var reg_x:Int = (opcode & 0x0F00) >> 8;
 						timers.sound_timer = registers[reg_x];
+						Log.trace('ld_st_reg [${reg_x}]');
 					}
 
 					case OpCodes.ADD_I_REG: {
 						var reg_x:Int = (opcode & 0x0F00) >> 8;
 						index_register += registers[reg_x];
+						Log.trace('add_i_reg [${reg_x}]');
 					}
 
 					case _: {
